f7371f03b8302761f6034fcade025e1a3f996e40
Tsiky ANDRIANAIVOSOA <tsikyandrianaivosu@gmail.com>
2022-03-23 21:52:28 +0300

    Change port number

--- server.js
+++ server.js
@@ -79,8 +79,9 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     // ========================
     // Listen
     // ========================
-    const isProduction = process.env.NODE_ENV === 'production'
-    const port = isProduction ? 7500 : 3000
+    //const isProduction = process.env.NODE_ENV === 'production'
+    const port = process.env.PORT || 5000
+    //isProduction ? 7500 : 3000
     app.listen(port, function () {
       console.log(`listening on ${port}`)
     })
