8344d457f26bb2d81b7823f4ca8c6671ec8d8a45
Tsiky ANDRIANAIVOSOA <tsikyandrianaivosu@gmail.com>
2022-04-04 21:06:01 +0300

    Drag and drop test

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { RoleGuardGuard } from './services/guards/role-guard.guard';
+import { CommandeListeComponent } from './commande-liste/commande-liste.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService, RoleGuardGuard], data: { expectedRoles: ['Client']}}
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService, RoleGuardGuard], data: { expectedRoles: ['Client']}},
+  { path: 'commandes', component: CommandeListeComponent, canActivate: [AuthGuardService]}
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,8 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { AuthenticationService } from './services/authentication.service';
 import { AuthGuardService } from './services/auth-guard.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommandeListeComponent } from './commande-liste/commande-liste.component';
 
 @NgModule({
   declarations: [
@@ -19,19 +22,22 @@ import { AuthGuardService } from './services/auth-guard.service';
     ProfileComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    CommandeListeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,      
-    CommonModule
+    CommonModule, 
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [  
     AuthenticationService, 
     AuthGuardService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
